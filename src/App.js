c96d26f38dfd109c5bf3146c2258f7647c5deb42
tapiasergio <sergiotapia782@gmail.com>
2022-09-25 00:36:57 -0300

    Tercer Commit
    base de desarrolladores

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import  Button  from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Nav from 'react-bootstrap/Nav';
+import Diseñadores from './Components/Diseñadores';
+//import { BrowserRouter as Router, RouteProps, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Card>
+          <Card.Header>
+            <h1>DISEÑADORES</h1>
+          </Card.Header>
+          <Card.Body>
+            <Nav variant="tabs" defaultActiveKey="#first">
+              <Nav.Item>
+                
+                <Nav.Link href="/src/Components/Diseñadores.js">Sergio</Nav.Link>
+                
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#2">Atilio</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#3">Fabian</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#4">Miguel</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#5">Chino</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#6">Fabricio</Nav.Link>
+              </Nav.Item>
+              
+            </Nav>
+            </Card.Body>
+        </Card>
   );
 }
 
