a3e1a6ad8bded60214c907110ddb4e49ffd8b31f
tapiasergio <sergiotapia782@gmail.com>
2022-09-25 00:33:10 -0300

    Tercer Commit

---
+++ src/Components/Diseñadores.js
@@ -0,0 +1,10 @@
+import Card from "react-bootstrap/Card";
+
+function Diseñadores() {
+    return(
+        <Card>
+            <Card.Header>hola</Card.Header>
+        </Card>
+    )
+}
+export default Diseñadores;
