a7db084ec3742752f60a5b0d1c69529fba46a09b
xAtilio00 <perprr4@gmail.com>
2022-10-16 23:53:16 -0300

    Agregacion de ganador y puntajes

--- src/Component/game/Game.js
+++ src/Component/game/Game.js
@@ -19,46 +19,61 @@ function randomImg(){
 function comparar(a,b){
     Players[0].opcion = a;
     Players[1].opcion = b;
-    if(a == b){
+    if(a === b){
         console.log("empate");
+        alert("empate");
     }else{
         if((a+b) % 2 === 0){//gana piedra
-            if(Players[0].opcion == 3){
-                Players[0].puntaje++;
-                console.log("gana jugador 1"+"-Puntaje: "+Players[0].puntaje++);
-                
+            if(Players[0].opcion === 3){
+                Players[0].puntaje+=1;
+                //console.log("gana jugador 1"+"-Puntaje: "+Players[0].puntaje++);
+                alert("El jugador 1 gana" + " // Puntaje Jugador 1: " + Players[0].puntaje + " // Puntaje Jugador 2: " + Players[1].puntaje);
             }else{
-                console.log("gana jugador 2"+"-Puntaje: "+Players[1].puntaje++);
-                Players[1].puntaje++;
+                //console.log("gana jugador 2"+"-Puntaje: "+Players[1].puntaje++);
+                Players[1].puntaje+=1;
+                alert("El jugador 2 gana" + " // Puntaje Jugador 1: " + Players[0].puntaje + "// Puntaje Jugador 2: " + Players[1].puntaje);
             }
-        }else if((a+b) % 2 == 1){ //gana tijera o gana papel
-            if(Players[0].opcion == 1 || Players[0].opcion == 3){
-                console.log("gana jugador 1"+"-Puntaje: "+Players[0].puntaje++);
-                Players[0].puntaje++;
+        }else if((a+b) % 2 === 1){ //gana tijera o gana papel
+            if(Players[0].opcion === 1 || Players[0].opcion === 3){
+                //console.log("gana jugador 1"+"-Puntaje: "+Players[0].puntaje++);
+                Players[0].puntaje+=1;
+                alert("El jugador 1 gana" + " // Puntaje Jugador 1: " + Players[0].puntaje + " // Puntaje Jugador 2: " + Players[1].puntaje);
             }else{
-                console.log("gana jugador 2"+"-Puntaje: "+Players[1].puntaje++);
-                Players[1].puntaje++;
+                //console.log("gana jugador 2"+"-Puntaje: "+Players[1].puntaje++);
+                Players[1].puntaje+=1;
+                alert("El jugador 2 gana" + " // Puntaje Jugador 1: " + Players[0].puntaje + " // Puntaje Jugador 2: " + Players[1].puntaje);
             }
         }
     }
 
 }
 
-const Numbers = () => {   
+
+const Numbers = () => {
     const navigate = useNavigate();
+
     return(
         <>
             <Button onClick={() => navigate("/")} size="sm">
                     Regresar al Inicio
             </Button>
-            <div className='estilo'>
-                <Imagen id="id1" src={Pregunta} style={{width: '20%'}}/>
-                <Button onClick={randomImg} variant="outline-primary">Gerar Número</Button>{''}
-                <Imagen id="id2" src={Pregunta} style={{width: '20%'}}/>    
+            <div className='estilo2'>
+                <h1 id='auxiliar'>{Players[0].name}</h1>
+                <h1 >{Players[1].name}</h1>
             </div>
-            
+
+            <div className='estilo'>
+
+                <Imagen id="id1" src={Pregunta} style={{width: '20%'}}/>
+
+                <Button onClick={ randomImg } variant="outline-primary">Gerar Número</Button>{''}
+                <Imagen id="id2" src={Pregunta} style={{width: '20%'}}/>
+
+            </div>
+
         </>
     );
+
 
 }
 
